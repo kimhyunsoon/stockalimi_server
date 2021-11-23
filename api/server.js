c5cf91bc77d398f4ce6ec0f6488523a706ab09eb
soon <sudosoon@gmail.com>
2021-11-23 13:58:42 +0900

    Docker copmose 1

---
+++ api/server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 4000;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST, ()=>{
+  console.log('80:4000 동작중');
+});
