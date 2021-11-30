a236c44cac3a9e792196ce5ed2c8609d321caf8f
soon <sudosoon@gmail.com>
2021-11-30 11:20:47 +0900

    cors 허용

--- api/server.js
+++ api/server.js
@@ -6,12 +6,12 @@ const PORT = 4040;
 const app = express();
 
 //cors
-// const cors = require('cors');
+const cors = require('cors');
 // let corsOption = {
 //   origin: 'http://139.150.73.190:80',
 //   credential: true,
 // }
-// app.use(cors(corsOption));
+app.use(cors());
 
 //json
 app.use(express.json());
