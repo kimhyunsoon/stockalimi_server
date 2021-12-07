7a4ca9be283186c7468de641d494a35134fa2cec
soon <sudosoon@gmail.com>
2021-12-07 19:47:57 +0900

    api 비동기로 변경

--- api/server.js
+++ api/server.js
@@ -66,11 +66,12 @@ app.post('/joinUser', async (req, res) => {
   //사용자 정보 저장은 클라이언트 앱 토큰 검증 후 처리함
   admin.messaging().send(tokenCheck)
     .then(result => { //토큰으로 푸쉬알림 발송시도가 성공하면 (앱에서 수신하지는 않음)
-      let r = DBevent.CreateUser(req.body);
+      let r = await DBevent.CreateUser(req.body);
       res.send(r); //성공시 true 실패시 'err' 반환
     })
     .catch(e => {
       err(e);
+      res.send('err');
     })
   
 })
@@ -88,11 +89,12 @@ app.post('/updateUser', async (req, res) => {
   //사용자 정보 저장은 클라이언트 앱 토큰 검증 후 처리함
   admin.messaging().send(tokenCheck)
     .then(result => { //토큰으로 푸쉬알림 발송시도가 성공하면 (앱에서 수신하지는 않음)
-      let r = DBevent.UpdateUser(req.body);
+      let r = await DBevent.UpdateUser(req.body);
       res.send(r); //성공시 true 실패시 'err' 반환
     })
     .catch(e => {
       err(e);
+      res.send('err');
     })
   
 })
