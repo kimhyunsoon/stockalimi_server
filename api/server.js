32ec1f3603fe5718716c865849357a0876e58e72
soon <sudosoon@gmail.com>
2021-12-09 14:02:00 +0900

    push test 5

--- api/server.js
+++ api/server.js
@@ -4,7 +4,11 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 
-const io = require('socket.io')(server);
+const io = new Server(http, {
+  cors: {
+    origin: ['http://localhost:8080']
+  }
+})
 
 io.on('connection', function (socket) {
   console.log(socket.id, 'Connected');
