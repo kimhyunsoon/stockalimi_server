23f58c4369319c9d02de94e6c65f9964138ee6be
soon <sudosoon@gmail.com>
2021-12-13 16:16:57 +0900

    update

--- api/src/DBinfo.js
+++ api/src/DBinfo.js
@@ -1,9 +1,6 @@
 module.exports = {
   barunalim : {
-    host : 'db', // 로컬은 localhost, 서버는 'db'
-    port: '3306',
-    user: 'master',
-    password: 'qkfmsgks0',
-    database: 'barunalim',
+    host : 'localhost', // 로컬은 localhost, 서버는 'db'
+
   },
 }
