6f2501c4c2a548ad40e1cc02cd1f7ea70adf1b36
soon <sudosoon@gmail.com>
2021-12-23 20:23:34 +0900

    db event update

--- api/server.js
+++ api/server.js
@@ -26,18 +26,20 @@ admin.initializeApp({
   credential: admin.credential.cert(serAccount),
 })
 
-//푸쉬알림발송
-app.put('/notification', (req, res) =>{
+//@푸쉬알림 발송
+app.put('/notification', async (req, res) =>{
   const title = req.body.title;
   const body = req.body.body;
-  const topic = req.body.topic;
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
 
-  //푸쉬알림발송은 약속된 문자열 확인 후 처리함
-  if (req.headers.api_code == 'XvBdJvufBnX4RIAuIgWAPGxA') {
-    log('topic:'+topic+', title:'+title+', body:'+body);
+  log(`put : /notification, ${app}`);
 
-    //topic 구독중인 앱에 firebase 알림 발송 (background)
-    const topicString = "'"+topic+"' in topics";
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if(checkResult === true) {
+    //앱에 firebase 알림 발송 (background)
+    const topicString = "'"+app+"' in topics";
     const msg = {
       notification: {
         title: title,
@@ -47,107 +49,257 @@ app.put('/notification', (req, res) =>{
     }
     admin.messaging().send(msg)
       .then(result => {
-        log('fcm : ' + result);
-        res.send('fcm success');
+        log(`FCM SUCCESS`);
+        res.send(`FCM SUCCESS`);
       })
       .catch(err =>{
-        err(err);
-        res.send(r);
+        err(`FCM ERROR`);
+        console.log(err);
+        res.send(`FCM ERROR`);
       })
   
     //앱에 socket 알림 발송 (foreground)
-    io.emit(topic, msg.notification);
-    log('socket : "'+topic+'" complete');
+    io.emit(app, msg.notification);
+    log(`SOCKET SUCCESS`);
+
+    //알림 발송 기록 저장
+    let historyResult = await DBevent.createNotificationHistory(title, body, app);
+    log(`${historyResult}`);
   } else {
-    res.send('err');
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 err 반환
   }
 })
 
-//앱사용기간 만료여부 조회
-app.get('/expiration/:app_code/:date/:phone', async (req, res) => {
-  const app_code = req.params.app_code;
+//@사용자 만료 검사
+app.get('/expiration/:date/:phone', async (req, res) => {
   const date = req.params.date;
   const phone = req.params.phone;
-  log(`get : /expiration/${app_code}/${date}/${phone}`);
-  //만료 여부 조회는 약속된 문자열 확인 후 처리함
-  if (req.headers.api_code == 'XvBdJvufBnX4RIAuIgWAPGxA') {
-    let r = await DBevent.UserExpirationCheck(app_code, date, phone);
-    res.send(r); //마감되었으면 false, 아니면 true, 에러시 'err' 반환
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
+
+  log(`get : /expiration/${app}/${date}/${phone}`);
+
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if (checkResult === true) {
+    let r = await DBevent.userExpirationCheck(app, date, phone);
+    res.send(r); //json 객체 반환
+    /*
+    {
+      result : 만료일이 남아있으면 true 아니면 false
+      expDate : 만료일자 ('YYYY-MM-DD')
+      dDayCnt : 남은일자
+    }
+    */
   } else {
-    res.send('err'); //약속어가 다르면 err 반환
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 403 반환
   }
 })
 
-//사용자 정보 저장
+//@사용자 정보 등록
 app.post('/user', async (req, res) => {
+  /* req.body
+  {
+    "name":사용자이름,
+    "phone":사용자전화번호,
+    "app_valid":만료일수,
+    "app_token":앱토큰
+  }
+ */
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
+
   log('post : /user');
-  const tokenCheck = {
-    data: {
-      message: "check" //푸쉬할 메시지
-    },
-    token: req.headers.apptoken,
-  };
 
-  //사용자 정보 저장은 클라이언트 앱 토큰 검증 후 처리함
-  admin.messaging().send(tokenCheck)
-    .then( async result => { //토큰으로 푸쉬알림 발송시도가 성공하면 (앱에서 수신하지는 않음)
-      log('apptoken check: '+ req.headers.apptoken);
-      let r = await DBevent.CreateUser(req.body);
-      await res.send(r); //성공시 true 실패시 'err' 반환
-    })
-    .catch(e => {
-      err(e);
-      res.send('err');
-    })
-  
-})
-
-//사용자 정보 업데이트
-app.put('/user', async (req, res) => {
-  log('put : /user');
-  const tokenCheck = {
-    data: {
-      message: "check" //푸쉬할 메시지
-    },
-    token: req.headers.apptoken,
-  };
-
-  //사용자 정보 저장은 클라이언트 앱 토큰 검증 후 처리함
-  admin.messaging().send(tokenCheck)
-    .then( async result => { //토큰으로 푸쉬알림 발송시도가 성공하면 (앱에서 수신하지는 않음)
-      let r = await DBevent.UpdateUser(req.body);
-      await res.send(r); //성공시 true 실패시 'err' 반환
-    })
-    .catch(e => {
-      err(e);
-      res.send('err');
-    })
-})
-
-//전화번호 중복체크
-app.get('/phone/:number/:app', async (req, res)=>{
-  const number = req.params.number;
-  const app = decodeURI(req.params.app);
-  
-  log('get : /phone/'+number+'/'+app);
-  //전화번호 중복체크는 약속된 문자열 확인 후 처리함
-  if (req.headers.api_code == 'XvBdJvufBnX4RIAuIgWAPGxA') {
-    let r = await DBevent.DuplicatePhoneNumberCheck(number, app)
-    res.send(r); //없으면 true 있으면 false, 에러시 'err' 반환
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if(checkResult === true) {
+    let r = await DBevent.createUser(req.body, app);
+    res.send(r); 
+    //사용자 정보 등록 후 성공시 true 실패시 400 반환
   } else {
-    res.send('err'); //약속어가 다르면 err 반환
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 403 반환
   }
 })
 
-//크롤링 데이터 요청
-app.get('/stock', (req, res) =>{
+//@전화번호 중복 검사
+app.get('/phone/:number', async (req, res)=>{
+  const number = req.params.number;
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
+
+  log(`get : /user/${number}`);
+
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if(checkResult === true) {
+    let r = await DBevent.duplicatePhoneNumberCheck(number, app);
+    res.send(r); 
+    //전화번호 중복 검사 후 없으면 true 있으면 false, 실패시 400 반환
+  } else {
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 403 반환
+  }
+})
+
+//@크롤링 데이터 요청
+app.get('/stock', async (req, res) =>{
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
+
   log('get : /stock');
-  //크롤링 데이터 요청의 경우는 약속된 문자열 확인 후 처리함
-  if (req.headers.api_code == 'XvBdJvufBnX4RIAuIgWAPGxA') {
+
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if(checkResult === true) {
     res.send(CrowlingEvent.totalData);
   } else {
-    res.send('err'); //약속어가 다르면 'err' 반환
-  } 
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 403 반환
+  }
+})
+
+//@사용자 만료일 갱신
+app.put('/user/expiration', async (req, res) => {
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
+  const phone = req.body.phone;
+  const date = req.body.date;
+
+  log('put : /user/prospective');
+
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if(checkResult === true) {
+    let r = await DBevent.updateUserExpDate(phone, app, date);
+    res.send(r);
+    //사용자 만료일 갱신 후 성공시 true 실패시 400 반환
+  } else {
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 403 반환
+  }
+})
+
+//@사용자 가망여부 갱신
+app.put('/user/prospective', async (req, res) => {
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
+  const phone = req.body.phone;
+  const bool = req.body.bool == 'true' ? true : false;
+
+  log('put : /user/prospective');
+
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if(checkResult === true) {
+    let r = await DBevent.updateUserProspective(phone, app, bool);
+    res.send(r);
+    //사용자 가망여부 갱신 후 성공시 true 실패시 400 반환
+  } else {
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 403 반환
+  }
+})
+
+//@사용자 목록 조회 (앱 별)
+app.get('/user/list', async (req, res) => {
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
+
+  log('get : /user/list');
+
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if(checkResult === true) {
+    let r = await DBevent.userList(app);
+    res.send(r); //조회목록 반환
+  } else {
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 403 반환
+  }
+})
+
+//@문의 요청 등록
+app.post('/contact', async (req, res) => {
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
+  const name = req.body.name;
+  const phone = req.body.phone;
+
+  log('post : /contact');
+
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if(checkResult === true) {
+    let r = await DBevent.createContactList(name, phone, app);
+    res.send(r); 
+    //문의요청 등록 후 성공시 true 실패시 400 반환
+  } else {
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 403 반환
+  }
+})
+
+//@문의 메모 갱신
+app.put('/contact/memo/:seq', async (req, res) => {
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
+  const seq = req.params.seq;
+  const memo = req.body.memo;
+
+  log('put : /contact/memo');
+
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if(checkResult === true) {
+    let r = await DBevent.updateContactMemo(seq, memo);
+    res.send(r); 
+    //문의 메모 갱신 후 성공시 true 실패시 400 반환
+  } else {
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 403 반환
+  }
+})
+
+//@문의 답변 여부 갱신
+app.put('/contact/answer/:seq', async (req, res) => {
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
+  const seq = req.params.seq;
+
+  log('put : /contact/answer');
+
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if(checkResult === true) {
+    let r = await DBevent.updateContactAnswer(seq);
+    res.send(r); 
+    //문의 답변 여부 갱신 후 성공시 true 실패시 400 반환
+  } else {
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 403 반환
+  }
+})
+
+//@문의 목록 조회 (앱 별)
+app.get('/contact/list', async (req, res) => {
+  const app = req.headers.appcode;
+  const key = req.headers.apikey;
+
+  log('get : /contact/list');
+
+  //앱 등록 여부 검사
+  let checkResult = await DBevent.validAppCheck(app, key);
+  if(checkResult === true) {
+    let r = await DBevent.contactList(app);
+    res.send(r); //조회목록 반환
+  } else {
+    log(`등록되지 않은 앱 : ${app}/${key}`);
+    res.send('403'); //등록된 앱이 아니면 403 반환
+  }
 })
 
 server.listen(4040, ()=>{
--- api/src/DBevent.js
+++ api/src/DBevent.js
@@ -15,99 +15,260 @@ const pool = mariadb.createPool({
   connectionLimit: 5
 });
 
-
-//전화번호 중복체크
-const DuplicatePhoneNumberCheck = async (number, app) => {
+//엡 등록 여부 검사
+const validAppCheck = async (app_code, api_key) => {
   let conn;
   try {
     conn = await pool.getConnection();
     const res = await conn.query(
-      "SELECT COUNT(*) as cnt FROM user_information WHERE phone='" + number + "' AND app_code= '" + app + "'"
+      `SELECT COUNT(*) as cnt FROM app_list WHERE app_code='${app_code}' AND api_key='${api_key}'`
     );
-    if (res[0].cnt == 0){
-      log(number + "/" + app+", 등록되어있지않음")
-    } else {
-      log(number + "/" + app+", 등록되어있음")
-    }
-    return await res[0].cnt == 0; // 있으면 false, 없으면 true 반환
+    return await res[0].cnt == 1; 
+    //유효한 앱이면 true, 아니면 false 반환
   } catch (e) {
-    err('DBevent : 전화번호 중복확인');
+    err('DBevent ERROR : validAppCheck');
     console.log(e);
-    return await "err"; // 실패시 'err'
+    return '400'; // 실패시 400
   } finally {
     if (conn) conn.release();
   }
 }
 
-//사용자 정보 저장
-const CreateUser = async user => {
+
+//전화번호 중복 검사
+const duplicatePhoneNumberCheck = async (number, app_code) => {
   let conn;
   try {
     conn = await pool.getConnection();
     const res = await conn.query(
-      "INSERT INTO user_information (name, phone, join_date, app_name) VALUE('"+user.name+"', '"+user.phone+"', NOW(), '"+user.app_name+"')"
+      `SELECT COUNT(*) as cnt FROM user_information WHERE phone='${number}' AND app_code= '${app_code}'`
     );
-    log(user.name+', '+user.phone+' 등록완료');
-    return await true; //성공시 true
+    return await res[0].cnt == 0;
+    //전화번호 중복 검사 후 없으면 true 있으면 false 반환
   } catch (e) {
+    err('DBevent ERROR : duplicatePhoneNumberCheck');
     console.log(e);
-    err('DBevent : CreateUser');
-    return await 'err'; // 실패시 'err'
+    return '400'; // 실패시 400 반환
   } finally {
     if (conn) conn.release();
   }
 }
 
-//사용자 정보 업데이트
-const UpdateUser = async user => {
+//사용자 등록
+const createUser = async (user, app_code) => {
   let conn;
   try {
     conn = await pool.getConnection();
     const res = await conn.query(
-      "UPDATE user_information SET name='"+user.name+"', phone='"+user.phone+"', join_date=NOW() WHERE phone='"+user.phone+"' AND app_name='"+user.app_name+"'"
+      `INSERT INTO user_information (name, phone, join_date, expiration_date, app_code, app_token) VALUE('${user.name}', '${user.phone}', NOW(), DATE_FORMAT(DATE_ADD(NOW(), INTERVAL ${user.app_valid} DAY), '%Y-%m-%d 23:59:59'), '${app_code}', '${user.app_token}')`
     );
-    log(user.name+', '+user.phone+' 등록완료');
-    return await true; //성공시 true
+    return res.affectedRows == 1?true:'400'; //성공시 true
   } catch (e) {
+    err('DBevent ERROR : createUser');
     console.log(e);
-    err('DBevent : UpdateUser');
-    return await 'err'; // 실패시 'err'
+    return '400'; // 실패시 400
   } finally {
     if (conn) conn.release();
   }
 }
 
-//만료일 조회
-const UserExpirationCheck = async (app_code, date, phone) => {
+//사용자 가망 여부 갱신
+const updateUserProspective = async (phone, app_code, bool) => {
+  let conn;
+  const prospective_user = bool?1:0;
+  try {
+    conn = await pool.getConnection();
+    const res = await conn.query(
+      `UPDATE user_information SET prospective_user='${prospective_user}' WHERE phone='${phone}' AND app_code='${app_code}'`
+    );
+    return res.affectedRows == 1?true:'400'; //성공시 true
+  } catch (e) {
+    console.log(e);
+    err('DBevent ERROR : updateUserProspective');
+    return '400'; // 실패시 400
+  } finally {
+    if (conn) conn.release();
+  }
+}
+
+//사용자 만료일 갱신
+const updateUserExpDate = async (phone, app_code, date) => {
+  let conn;
+  const expDate = `${date.subtract(1, 'days').format('YYYY-MM-DD')} 23:59:59`;
+  try {
+    conn = await pool.getConnection();
+    const res = await conn.query(
+      `UPDATE user_information SET expiration_date='${expDate}' WHERE phone='${phone}' AND app_code='${app_code}'`
+    );
+    return res.affectedRows == 1?true:'400'; //성공시 true
+  } catch (e) {
+    console.log(e);
+    err('DBevent ERROR : updateUserExpDate');
+    return '400'; // 실패시 400
+  } finally {
+    if (conn) conn.release();
+  }
+}
+
+//사용자 만료 검사
+const userExpirationCheck = async (app_code, date, phone) => {
   let conn;
   try {
     conn = await pool.getConnection();
     const res = await conn.query(
-      "SELECT expiration_date FROM user_information WHERE phone='" + phone + "' AND app_code='" + app_code + "'"
+      `SELECT expiration_date FROM user_information WHERE phone='${phone}' AND app_code='${app_code}'`
     );
     if (res[0] === undefined){
-      log(app_code + "/" + phone+", 등록되어있지않음");
-      return await 'unregist';
+      return "400";
     } else {
-      const now = moment();
-      const expDate = moment(res[0].expiration_date);
-      const expTest = expDate.isAfter(now);
+      const now = moment(); 
+      const exp = moment(res[0].expiration_date);
 
-      log(expTest);
-      return await expTest; //만료일이 남아있으면 true 아니면 false 반환;
+      const nowYMD = moment(now.format('YYYY-MM-DD'));
+      const expYMD = moment(exp.add(1, 'days').format('YYYY-MM-DD'));
+
+      let dDayCnt = 0;
+      if(exp.isAfter(now)){
+        remaining_date = expYMD.diff(nowYMD, 'days');
+        //남은일자 계산
+      }
+      const reData = {
+        result : exp.isAfter(now), // 만료일이 남아있으면 true 아니면 false
+        expDate : expYMD, // 만료일자 ('YYYY-MM-DD')
+        dDayCnt : dDayCnt, // 남은일자
+      }
+      return reData;
     }
   } catch (e) {
-    err(`DBevent : 만료일 조회(${app_code}/${date}/${phone})`);
+    err(`DBevent ERROR : userExpirationCheck(${app_code}/${date}/${phone})`);
     console.log(e);
-    return await "err"; // 실패시 'err'
+    return "400"; // 실패시 400
   } finally {
     if (conn) conn.release();
   }
 }
 
+//사용자 목록 조회 (앱 별)
+const userList = async (app_code) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const res = await conn.query(
+      `SELECT seq, name, phone, join_date, expiration_date, prospective_user FROM user_information WHERE app_code='${app_code}'`
+    );
+    return res;
+  } catch (e) {
+    err(`DBevent ERROR : userList(${app_code})`);
+    console.log(e);
+    return "400"; // 실패시 400
+  } finally {
+    if (conn) conn.release();
+  }
+}
+
+//알림 발송 기록 저장
+const createNotificationHistory = async (title, body, app_code) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const res = await conn.query(
+      `INSERT INTO notification_history (title, body, push_date, app_code) VALUE('${title}', ${body}, NOW(), ${app_code})`
+    );
+    return 'SUCCESS'; //성공시 SUCCESS
+  } catch (e) {
+    err('DBevent ERROR : createNotificationHistory');
+    console.log(e);
+    return 'ERROR'; // 실패시 ERROR
+  } finally {
+    if (conn) conn.release();
+  }
+}
+
+//문의 요청 등록
+const createContactList = async (name, phone, app_code) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const res = await conn.query(
+      `INSERT INTO contact_list (app_code, name, phone, contact_date) VALUE('${app_code}', ${name}, ${phone} ,NOW())`
+    );
+    return res.affectedRows == 1?true:'400'; //성공시 true
+  } catch (e) {
+    err('DBevent ERROR : createContactList');
+    console.log(e);
+    return '400'; // 실패시 400
+  } finally {
+    if (conn) conn.release();
+  }
+}
+
+//문의 메모 갱신
+const updateContactMemo = async (seq, memo) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const res = await conn.query(
+      `UPDATE contact_list SET memo='${memo}' WHERE seq='${seq}'`
+    );
+    return res.affectedRows == 1?true:'400'; //성공시 true
+  } catch (e) {
+    console.log(e);
+    err('DBevent ERROR : updateContactMemo');
+    return '400'; // 실패시 400
+  } finally {
+    if (conn) conn.release();
+  }
+}
+
+//문의 답변 여부 갱신
+const updateContactAnswer = async (seq) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const res = await conn.query(
+      `UPDATE contact_list SET answer_flag=1 WHERE seq='${seq}'`
+    );
+    return res.affectedRows == 1?true:'400'; //성공시 true
+  } catch (e) {
+    console.log(e);
+    err('DBevent ERROR : updateContactAnswer');
+    return '400'; // 실패시 400
+  } finally {
+    if (conn) conn.release();
+  }
+}
+
+//문의 목록 조회 (앱 별)
+const contactList = async (app_code) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const res = await conn.query(
+      `SELECT seq, user_name, user_phone, contact_date, answer_flag, memo FROM contact_list WHERE app_code='${app_code}'`
+    );
+    return res;
+  } catch (e) {
+    err(`DBevent ERROR : contactList(${app_code})`);
+    console.log(e);
+    return "400"; // 실패시 400
+  } finally {
+    if (conn) conn.release();
+  }
+}
+
+
+
 module.exports = {
-  DuplicatePhoneNumberCheck : DuplicatePhoneNumberCheck,
-  CreateUser : CreateUser,
-  UpdateUser : UpdateUser,
-  UserExpirationCheck : UserExpirationCheck
+  validAppCheck : validAppCheck,
+  duplicatePhoneNumberCheck : duplicatePhoneNumberCheck,
+  createUser : createUser,
+  userExpirationCheck : userExpirationCheck,
+  createNotificationHistory : createNotificationHistory,
+  createContactList : createContactList,
+  updateContactMemo : updateContactMemo,
+  updateUserProspective : updateUserProspective,
+  updateUserExpDate : updateUserExpDate,
+  userList : userList,
+  updateContactAnswer : updateContactAnswer,
+  contactList : contactList,
 }
