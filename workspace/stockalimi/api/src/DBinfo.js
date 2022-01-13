4c30e3d40cb10e7814d29fd762001b90834fcf1b
soon <sudosoon@gmail.com>
2022-01-13 16:43:13 +0900

    서버 구조 변경

---
+++ workspace/stockalimi/api/src/DBinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stockalimi : {
+    host : '139.150.73.190', // 로컬은 localhost, 서버는 'db'
+    port: '3306',
+    user: 'master',
+    password: 'qkfmsgks0',
+    database: 'stockalimi',
+  },
+}
