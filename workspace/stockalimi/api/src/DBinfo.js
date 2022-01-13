79c8b53bd0e6bb32a45aed5e73156af582771fa0
soon <sudosoon@gmail.com>
2022-01-13 17:19:17 +0900

    예외처리

--- workspace/stockalimi/api/src/DBinfo.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stockalimi : {
-    host : '139.150.73.190', // 로컬은 localhost, 서버는 'db'
-    port: '3306',
-    user: 'master',
-    password: 'qkfmsgks0',
-    database: 'stockalimi',
-  },
-}
